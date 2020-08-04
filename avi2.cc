205cff2e24aa1e564d99788244603d774b89f81e
fand <fand.gmork@gmail.com>
2020-08-04 10:34:22 +0900

    Get input file path from CLI argument

--- avi2.cc
+++ avi2.cc
@@ -3,17 +3,20 @@
 #include <opencv2/objdetect/objdetect.hpp>
 #include <time.h>
 #include <algorithm>
+#include <iostream>
 
 #define SCALE 2.0
 
 using namespace cv;
 
-int vibe(int range, double ratio_min, double ratio_max){
-    return (ratio_min * range) + (rand() % (int)((ratio_max-ratio_min)*range));
+int vibe(int range, double ratio_min, double ratio_max)
+{
+    return (ratio_min * range) + (rand() % (int)((ratio_max - ratio_min) * range));
 }
 
-Rect* getRoi(Mat* img, CvRect* faceRect){
-                
+Rect *getRoi(Mat *img, CvRect *faceRect)
+{
+
     int x = (faceRect->x + vibe(faceRect->width, -0.2, 0.1)) * SCALE;
     int y = (faceRect->y + vibe(faceRect->height, -0.2, 0.2)) * SCALE;
     int w = vibe(faceRect->width, 0.8, 1.3) * SCALE;
@@ -21,23 +24,28 @@ Rect* getRoi(Mat* img, CvRect* faceRect){
 
     x = min(max(x, 0), img->cols);
     y = min(max(y, 0), img->rows);
-    w = min(max(w, 0), img->cols - x-1);
-    h = min(max(h, 0), img->rows - y-1);
+    w = min(max(w, 0), img->cols - x - 1);
+    h = min(max(h, 0), img->rows - y - 1);
 
-    return new Rect(x,y,w,h);
+    return new Rect(x, y, w, h);
 }
 
-Mat glitch(Mat img, std::vector<uchar>* buf, std::vector<uchar>* buf2, std::vector<int>* params){
+Mat glitch(Mat img, std::vector<uchar> *buf, std::vector<uchar> *buf2, std::vector<int> *params)
+{
 
     std::vector<uchar>::iterator it, it2;
 
-    imencode(".jpg", img, *buf, *params);    // encode to jpg
+    imencode(".jpg", img, *buf, *params); // encode to jpg
 
-    buf2->resize( buf->size());
-    for(it = buf->begin(), it2 = buf2->begin(); it != buf->end(); ++it, ++it2){
-        if(*it == 'x' || *it == 't'){
-            *it2 = rand()%10;
-        }else{
+    buf2->resize(buf->size());
+    for (it = buf->begin(), it2 = buf2->begin(); it != buf->end(); ++it, ++it2)
+    {
+        if (*it == 'x' || *it == 't')
+        {
+            *it2 = rand() % 10;
+        }
+        else
+        {
             *it2 = *it;
         }
     }
@@ -46,25 +54,36 @@ Mat glitch(Mat img, std::vector<uchar>* buf, std::vector<uchar>* buf2, std::vect
     return imdecode(Mat(*buf2), 1);
 }
 
-inline CvSeq* getFaces(IplImage* frame_copy, CvHaarClassifierCascade* cvHCC, CvMemStorage* cvMStr){
+inline CvSeq *getFaces(IplImage *frame_copy, CvHaarClassifierCascade *cvHCC, CvMemStorage *cvMStr)
+{
 
     // グレースケール化, ヒストグラムの均一化
-    IplImage* gray = cvCreateImage(cvSize(frame_copy->width, frame_copy->height), IPL_DEPTH_8U, 1);
-    IplImage* detect_frame = cvCreateImage(cvSize((frame_copy->width / SCALE),
+    IplImage *gray = cvCreateImage(cvSize(frame_copy->width, frame_copy->height), IPL_DEPTH_8U, 1);
+    IplImage *detect_frame = cvCreateImage(cvSize((frame_copy->width / SCALE),
                                                   (frame_copy->height / SCALE)),
                                            IPL_DEPTH_8U, 1);
     cvCvtColor(frame_copy, gray, CV_BGR2GRAY);
     cvResize(gray, detect_frame, CV_INTER_LINEAR);
     cvEqualizeHist(detect_frame, detect_frame);
-    
+
     return cvHaarDetectObjects(detect_frame, cvHCC, cvMStr, 1.1, 2, CV_HAAR_DO_CANNY_PRUNING, cvSize(30, 30));
 }
 
-int main(int argc, char **argv){
+int main(int argc, char **argv)
+{
+    // Get input file name
+    if (argc != 3)
+    {
+        std::cout << "Usage: " << std::endl
+                  << "  $ glitch [INPUT_FILE] [OUTPUT_FILE]" << std::endl;
+        return -1;
+    }
+    char *input_file = argv[1];
+    char *output_file = argv[2];
 
-    CvCapture *capture = NULL;  // capture object
-    IplImage *frame = 0;        // キャプチャ画像用ポインタ
-    IplImage *frame_copy = 0;   // キャプチャ画像のコピー用ポインタ
+    CvCapture *capture = NULL; // capture object
+    IplImage *frame = 0;       // キャプチャ画像用ポインタ
+    IplImage *frame_copy = 0;  // キャプチャ画像のコピー用ポインタ
     int input_key;
 
     // buffer for encode/decode
@@ -75,33 +94,36 @@ int main(int argc, char **argv){
     params[1] = 10;
 
     srand((unsigned int)time(NULL));
-    
-    CvHaarClassifierCascade* cvHCC = (CvHaarClassifierCascade*)cvLoad("haarcascade_frontalface_default.xml");
-    CvMemStorage* cvMStr = cvCreateMemStorage(0);
-    CvSeq* face;
-    
-    capture = cvCaptureFromFile("/home/fand/cv/apple.avi");              // ファイル読み込み
-    double width = cvGetCaptureProperty (capture, CV_CAP_PROP_FRAME_WIDTH);
-    double height = cvGetCaptureProperty (capture, CV_CAP_PROP_FRAME_HEIGHT);
-    double fps = cvGetCaptureProperty (capture, CV_CAP_PROP_FPS);
-    
-    CvVideoWriter* vidWriter = cvCreateVideoWriter("out.avi", CV_FOURCC('M','J','P','G'), fps, cvSize(width, height), 1);
 
+    CvHaarClassifierCascade *cvHCC = (CvHaarClassifierCascade *)cvLoad("haarcascade_frontalface_default.xml");
+    CvMemStorage *cvMStr = cvCreateMemStorage(0);
+    CvSeq *face;
 
-    namedWindow("Video", CV_WINDOW_AUTOSIZE|CV_WINDOW_FREERATIO);      // ウィンドウ作成
+    capture = cvCaptureFromFile(input_file); // ファイル読み込み
+    double width = cvGetCaptureProperty(capture, CV_CAP_PROP_FRAME_WIDTH);
+    double height = cvGetCaptureProperty(capture, CV_CAP_PROP_FRAME_HEIGHT);
+    double fps = cvGetCaptureProperty(capture, CV_CAP_PROP_FPS);
+
+    CvVideoWriter *vidWriter = cvCreateVideoWriter(output_file, CV_FOURCC('M', 'J', 'P', 'G'), fps, cvSize(width, height), 1);
+
+    namedWindow("Video", CV_WINDOW_AUTOSIZE | CV_WINDOW_FREERATIO); // ウィンドウ作成
     cvMoveWindow("Video", 300, 200);
 
-
     // main loop
-    while (1) {
-        frame = cvQueryFrame (capture);
-        if(frame == NULL) break;
+    while (1)
+    {
+        frame = cvQueryFrame(capture);
+        if (frame == NULL)
+            break;
 
         // フレームコピー用イメージ生成
         frame_copy = cvCreateImage(cvSize(frame->width, frame->height), IPL_DEPTH_8U, frame->nChannels);
-        if(frame->origin == IPL_ORIGIN_TL) {
+        if (frame->origin == IPL_ORIGIN_TL)
+        {
             cvCopy(frame, frame_copy);
-        } else {
+        }
+        else
+        {
             cvFlip(frame, frame_copy);
         }
 
@@ -109,55 +131,56 @@ int main(int argc, char **argv){
         // face = getFaces(frame_copy, cvHCC, cvMStr);            // face detect
 
         // 読み込んだ画像のグレースケール化、及びヒストグラムの均一化を行う
-        IplImage* gray = cvCreateImage(cvSize(frame_copy->width, frame_copy->height), IPL_DEPTH_8U, 1);
-        IplImage* detect_frame = cvCreateImage(cvSize((frame_copy->width / SCALE), (frame_copy->height / SCALE)), IPL_DEPTH_8U, 1);
+        IplImage *gray = cvCreateImage(cvSize(frame_copy->width, frame_copy->height), IPL_DEPTH_8U, 1);
+        IplImage *detect_frame = cvCreateImage(cvSize((frame_copy->width / SCALE), (frame_copy->height / SCALE)), IPL_DEPTH_8U, 1);
         cvCvtColor(frame_copy, gray, CV_BGR2GRAY);
         cvResize(gray, detect_frame, CV_INTER_LINEAR);
         cvEqualizeHist(detect_frame, detect_frame);
 
         // 画像中から検出対象の情報を取得する
-        face = cvHaarDetectObjects(detect_frame, cvHCC, cvMStr, 1.1, 2, CV_HAAR_DO_CANNY_PRUNING, cvSize(30, 30) );
+        face = cvHaarDetectObjects(detect_frame, cvHCC, cvMStr, 1.1, 2, CV_HAAR_DO_CANNY_PRUNING, cvSize(30, 30));
 
-        Mat img(frame_copy);                                   // convert frame to Mat
-        Mat img_glitched = glitch(img, &buf, &buf2, &params);  // glitch
-        
+        Mat img(frame_copy);                                  // convert frame to Mat
+        Mat img_glitched = glitch(img, &buf, &buf2, &params); // glitch
+
         // 検出した位置にglitch 画像を貼り付ける
-        for (int i = 0; i < face->total; i++) {
-            CvRect* faceRect = (CvRect*)cvGetSeqElem(face, i);
-/*
+        for (int i = 0; i < face->total; i++)
+        {
+            CvRect *faceRect = (CvRect *)cvGetSeqElem(face, i);
+            /*
             // roiの範囲で、img_glitched を img に貼り付け
             Rect* roi_src = getRoi(&img, faceRect);
             Rect* roi_dst = getRoi(&img, faceRect);
             Mat mat_src = img_glitched(*roi_src);
             Mat mat_dst = img(*roi_dst);
 
-            resize(mat_src, mat_dst, mat_dst.size(), INTER_CUBIC); 
+            resize(mat_src, mat_dst, mat_dst.size(), INTER_CUBIC);
 */
             // roiの範囲で、img_glitched を img に貼り付け
-            Rect* roi = getRoi(&img, faceRect);
+            Rect *roi = getRoi(&img, faceRect);
             Mat mat_src = img_glitched(*roi);
             Mat mat_dst = img(*roi);
-            
+
             mat_src.copyTo(mat_dst);
 
-            delete(roi);
+            delete (roi);
         }
-        
-        imshow("Video", img);    // show img
 
+        imshow("Video", img); // show img
 
         IplImage writer = img;
         cvWriteFrame(vidWriter, &writer);
-        
-        input_key = cvWaitKey (10); // 終了キー入力待ち（タイムアウト付き）
-        if (input_key == 'e') {
+
+        input_key = cvWaitKey(10); // 終了キー入力待ち（タイムアウト付き）
+        if (input_key == 'e')
+        {
             break;
         }
     }
-    
-    cvReleaseCapture(&capture);  // delete capture
-    cvReleaseVideoWriter(&vidWriter); 
-    cvDestroyWindow("Capture");  // delete window
+
+    cvReleaseCapture(&capture); // delete capture
+    cvReleaseVideoWriter(&vidWriter);
+    cvDestroyWindow("Capture"); // delete window
 
     cvReleaseMemStorage(&cvMStr);
     cvReleaseHaarClassifierCascade(&cvHCC);
